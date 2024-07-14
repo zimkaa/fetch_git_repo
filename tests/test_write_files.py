1a3f3d39348c24c3f5dc351a8ff84926fa3f13e0
Anton Zimin <zimkaa87@gmail.com>
2024-07-14 14:50:11 +0500

    fix: auto linters format
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- tests/test_write_files.py
+++ tests/test_write_files.py
@@ -1,13 +1,10 @@
-from unittest.mock import AsyncMock
-from unittest.mock import MagicMock
-from unittest.mock import patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
-from tests.conftest import ORIGINAL_STRING
-from src.main import TEMP_PROJECT_FOLDER
-from src.main import _write_files
+from src.main import TEMP_PROJECT_FOLDER, _write_files
 from src.my_types import FileStructure
+from tests.conftest import ORIGINAL_STRING
 
 
 @pytest.mark.asyncio()
@@ -23,7 +20,9 @@ async def test_write_files_single_file(file_structure: FileStructure) -> None:
         mock_file = AsyncMock()
         mock_open.return_value.__aenter__.return_value = mock_file
         await _write_files([file_structure])
-        mock_open.assert_called_once_with(TEMP_PROJECT_FOLDER / file_structure.path, "w")
+        mock_open.assert_called_once_with(
+            TEMP_PROJECT_FOLDER / file_structure.path, "w"
+        )
         mock_file.write.assert_called_once_with(ORIGINAL_STRING)
 
 
