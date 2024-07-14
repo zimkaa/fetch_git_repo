066bb702ba1688ad4f7213298db2ea05d3a6e81d
Anton Zimin <zimkaa87@gmail.com>
2024-07-14 14:45:15 +0500

    feat: some refactoring
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 import asyncio
 import base64
 import hashlib
@@ -10,16 +9,17 @@ from pathlib import Path
 from typing import Final
 
 import aiofiles
-from aiohttp import ClientSession, ClientTimeout
+from aiohttp import ClientSession
+from aiohttp import ClientTimeout
 
-from src.my_types import FileStructure, ResponseStructure, TreeElement
+from src.my_types import FileStructure
+from src.my_types import ResponseStructure
+from src.my_types import TreeElement
 
 
 HEADERS: Final[dict] = {"accept": "application/json"}
 
-URL: Final[str] = (
-    "https://gitea.radium.group/api/v1/repos/radium/project-configuration/git/trees/HEAD?recursive=true"
-)
+URL: Final[str] = "https://gitea.radium.group/api/v1/repos/radium/project-configuration/git/trees/HEAD?recursive=true"
 
 TEMP_FOLDER = Path("/tmp")
 
@@ -45,7 +45,8 @@ async def _fetch_repo_structure(url: str, session: ClientSession) -> ResponseStr
 
 
 async def _download_file_structure(
-    elements: tuple[TreeElement, ...], session: ClientSession
+    elements: tuple[TreeElement, ...],
+    session: ClientSession,
 ) -> list[FileStructure]:
     file_structures = []
     for element in elements:
@@ -100,9 +101,7 @@ async def main() -> None:
                 _create_empty_file(item.path)
 
         batch_size = structure.total_count // 3  # 3 - stream
-        assert (
-            batch_size > 0
-        ), "Repo doesn't have enough files to load parallel 3 - way stream"
+        assert batch_size > 0, "Repo doesn't have enough files to load parallel 3 - way stream"
         urls_batch1, urls_batch2, urls_batch3 = batched(file_urls, batch_size + 1)
 
         file_structures: list[FileStructure] = []
